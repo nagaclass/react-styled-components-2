4b64e01aefed0b282f0ee6134663d864278db587
muhammad-naga <muhammad.naga.ali@gmail.com>
2021-07-09 17:06:18 +0200

    Adding loading spinner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import HomePage from "./pages";
+import { lazy, Suspense } from "react";
+import LoadingSpinner from "./components/LoadingSpinner";
+
 import ScrollTopTop from "./components/ScrollToTop";
+
+const HomePage = lazy(() => import("./pages"));
 
 const App = () => {
   return (
     <>
-      <HomePage />
+      <Suspense fallback={<LoadingSpinner />}>
+        <HomePage />
+      </Suspense>
       <ScrollTopTop />
     </>
   );
---
+++ src/components/LoadingSpinner/index.js
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-wrapper">
+      <div className="lds-dual-ring"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
